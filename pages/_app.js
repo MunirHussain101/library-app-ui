906a3d4ac7e1ba66dd1c4add9bc44b2b12db0d21
MunirHussain101 <munirhussaindev0@gmail.com>
2023-03-31 23:38:01 +0500

    inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(
+    <>
+      <Head>
+        <title>Book App</title>
+        <meta
+          name="Book App"
+          content="A book app that allows you to view modify and read books"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
